7eec5fddf8867d7530f16ab140e17aa73870ec6d
SiddTheCoder <siddthecoder@gmail.com>
2026-08-04 09:25:21 +0545

    refactor: remove community-related components and API endpoints

--- apps/web/src/app/(resident)/resident/community/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ResidentCommunityPageContent } from "@/app/_components/resident-community-page";
-
-export default function ResidentCommunityPage() {
-  return <ResidentCommunityPageContent />;
-}
